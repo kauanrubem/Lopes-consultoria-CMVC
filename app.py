a111c82fe44c2ca59f398d5c4292fbc58afc7f9a
kauanrubem <kauanrubem87@gmail.com>
2025-04-15 15:36:55 -0300

    Adicionando o docker

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def update_layout(selected_value):
     
 # Rodar o servidor
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, port=8080, host="0.0.0.0")  # Para rodar em qualquer IP
